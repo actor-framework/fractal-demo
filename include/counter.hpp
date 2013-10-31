d2d03533d925a6b87e3d0e5d419b73afa7781459
Raphael Hiesgen <hiesgen@mobi33.cpt.haw-hamburg.de>
2013-10-31 18:27:52 +0100

    counter class sends images to ui, skips dropped
    todo: adjust frame rate

--- include/counter.hpp
+++ include/counter.hpp
@@ -1,6 +1,9 @@
 #ifndef COUNTER_HPP
 #define COUNTER_HPP
 
+#include <set>
+#include <vector>
+
 #include "cppa/cppa.hpp"
 
 class counter : public cppa::event_based_actor {
@@ -12,7 +15,14 @@ class counter : public cppa::event_based_actor {
 
  private:
 
+    bool probe();
     void init(cppa::actor_ptr gui);
+
+    std::uint32_t m_next;
+    std::uint32_t m_delay; // in msec
+
+    std::set<std::uint32_t> m_dropped;
+    std::map<std::uint32_t, QByteArray> m_buffer;
 
 };
 
--- src/counter.cpp
+++ src/counter.cpp
@@ -1,24 +1,50 @@
 
+#include <chrono>
+#include <algorithm>
+
+#include <QByteArray>
+
 #include "cppa/cppa.hpp"
 
 #include "include/counter.hpp"
-
-#include "q_byte_array_info.hpp"
 
 using namespace std;
 using namespace cppa;
 
 
-counter::counter() { }
+counter::counter() : m_next(0), m_delay(300) { }
+
+bool counter::probe() {
+    auto i = m_dropped.find(m_next);
+    if (i == m_dropped.end()) {
+        return false;
+    }
+    else {
+        m_dropped.erase(i);
+        ++m_next;
+        return true;
+    }
+}
 
 void counter::init(actor_ptr gui) {
     become(
-        on(atom("image"), arg_match) >> [=] (uint32_t,
-                                             const QByteArray&) {
-            // todo save in buffer, track ips, adjust framerate
+        on(atom("tick")) >> [=] {
+            while (probe());
+            auto j = m_buffer.find(m_next);
+            if (j != m_buffer.end()) {
+                send(gui, move(j->second)); // todo is this valid?
+                m_buffer.erase(j);
+            }
+            delayed_send(self, chrono::milliseconds(m_delay), atom("tick"));
         },
-        on(atom("dropped"), arg_match) >> [=] (uint32_t) {
-            // todo find next available image
+        on(atom("image"), arg_match) >> [=] (uint32_t id,
+                                             const QByteArray& ba) {
+            m_buffer.insert(make_pair(id, ba));
+            // todo track ips, adjust framerate
+        },
+        on(atom("dropped"), arg_match) >> [=] (uint32_t id) {
+            if (id > m_next) { m_dropped.insert(id); }
+            else if (id == m_next) { ++m_next; }
         },
         others() >> [=] {
             cout << "[!!!] counter received unexpected message: '"
@@ -32,6 +58,7 @@ void counter::init() {
     trap_exit(true);
     become (
         on(atom("init"), arg_match) >> [=] (actor_ptr gui) {
+            delayed_send(self, chrono::milliseconds(m_delay), atom("tick"));
             init(gui);
         }
 

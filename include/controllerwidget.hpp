e57b3e78385e1898ad73142f4890d20520e53559
Alex Mantel <alexmantel93@gmail.com>
2014-09-14 21:09:36 +0200

    Add CAFs probe support

--- include/controllerwidget.hpp
+++ include/controllerwidget.hpp
@@ -20,7 +20,7 @@ class ControllerWidget : public caf::mixin::actor_widget<QWidget> {
 
     explicit ControllerWidget(QWidget *parent = nullptr, Qt::WindowFlags f = 0);
 
-    inline void set_controller(const cppa::actor& controller_actor) { m_controller = controller_actor; }
+    inline void set_controller(const caf::actor& controller_actor) { m_controller = controller_actor; }
     void initialize();
 
  public slots:
@@ -84,7 +84,7 @@ class ControllerWidget : public caf::mixin::actor_widget<QWidget> {
         time_current()->setNum(static_cast<int>(time));
     }
 
-    inline void set_fractal_types(const std::map<std::string,cppa::atom_value>& fractal_types) {
+    inline void set_fractal_types(const std::map<std::string,caf::atom_value>& fractal_types) {
         m_valid_fractal = fractal_types;
         while(drop_down_fractal_type() == nullptr); // wait for the UI
         for(auto& fractal : fractal_types) {
@@ -108,7 +108,7 @@ class ControllerWidget : public caf::mixin::actor_widget<QWidget> {
 
     std::vector<QString> m_res_strings;
     std::vector<std::pair<std::uint32_t, std::uint32_t>> m_resolutions;
-    std::map<std::string, cppa::atom_value> m_valid_fractal;
+    std::map<std::string, caf::atom_value> m_valid_fractal;
 };
 
 #endif // CONTROLLERWIDGET_HPP

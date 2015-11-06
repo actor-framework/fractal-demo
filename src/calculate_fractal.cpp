1fb8fd44b3dd9565b2784ee74c77d14933aec1f1
Joseph Noir <joseph.noir@gmail.com>
2015-11-06 15:56:57 +0100

    Update other fractal kernels

--- src/calculate_fractal.cpp
+++ src/calculate_fractal.cpp
@@ -122,10 +122,10 @@ constexpr const char* mandelbrot_kernel = R"__(
 
 // TODO: fix types and arguments
 constexpr const char* tricorn_kernel = R"__(
-  __kernel void calculate_fractal(__global float* config,
-                                  __global int*   output) {
-    float offset = config[7];
-    float rows   = config[8];
+  __kernel void calculate_fractal(__global float*    config,
+                                  __global unsigned short* output) {
+    unsigned offset = (unsigned) config[7];
+    unsigned rows   = (unsigned) config[8];
     unsigned x = get_global_id(0);
     unsigned y = get_global_id(1);
     if (y >= offset && y < (offset + rows)) {
@@ -136,10 +136,10 @@ constexpr const char* tricorn_kernel = R"__(
       float max_re = config[4];
       float min_im = config[5];
       float max_im = config[6];
-      float re_factor = (max_re-min_re)/(width-1);
-      float ifactor_ = (max_im-min_im)/(height-1);
-      float z_re = min_re + x*re_factor;
-      float z_im = max_im - y*ifactor_;
+      float re_factor = (max_re - min_re) / (width - 1);
+      float im_factor = (max_im - min_im) / (height - 1);
+      float z_re = min_re + x * re_factor;
+      float z_im = max_im - y * im_factor;
       float const_re = z_re;
       float const_im = z_im;
       unsigned cnt = 0;
@@ -147,22 +147,22 @@ constexpr const char* tricorn_kernel = R"__(
       do {
         float tmp_re = z_re;
         float tmp_im = z_im;
-        z_re = ( tmp_re*tmp_re - tmp_im*tmp_im ) + const_re;
+        z_re = ( tmp_re * tmp_re - tmp_im * tmp_im ) + const_re;
         z_im = -1 * ( 2 * tmp_re * tmp_im ) + const_im;
-        cond = z_re*z_re + z_im*z_im;
-        cnt ++;
+        cond = z_re * z_re + z_im * z_im;
+        ++cnt;
       } while (cnt < iterations && cond <= 4.0f);
-      output[x+y*width] = cnt;
+      output[x + (y - offset) * width] = cnt;
     }
   }
 )__";
 
 // TODO: fix types and arguments
 constexpr const char* burnship_kernel = R"__(
-  __kernel void calculate_fractal(__global float* config,
-                                  __global int*   output) {
-    float offset = config[7];
-    float rows   = config[8];
+  __kernel void calculate_fractal(__global float*    config,
+                                  __global unsigned short* output) {
+    unsigned offset = (unsigned) config[7];
+    unsigned rows   = (unsigned) config[8];
     unsigned x = get_global_id(0);
     unsigned y = get_global_id(1);
     if (y >= offset && y < (offset + rows)) {
@@ -173,12 +173,12 @@ constexpr const char* burnship_kernel = R"__(
       float max_re = config[4];
       float min_im = config[5];
       float max_im = config[6];
-      float re_factor = (max_re-min_re)/(width-1);
-      float ifactor_ = (max_im-min_im)/(height-1);
+      float re_factor = (max_re - min_re) / (width - 1);
+      float im_factor = (max_im - min_im) / (height - 1);
       unsigned x = get_global_id(0);
       unsigned y = get_global_id(1);
-      float z_re = min_re + x*re_factor;
-      float z_im = max_im - y*ifactor_;
+      float z_re = min_re + x * re_factor;
+      float z_im = max_im - y * im_factor;
       float const_re = z_re;
       float const_im = z_im;
       unsigned cnt = 0;
@@ -186,35 +186,36 @@ constexpr const char* burnship_kernel = R"__(
       do {
         auto tmp_re = z_re;
         auto tmp_im = z_im;
-        z_re = ( tmp_re*tmp_re - tmp_im*tmp_im ) - const_re;
+        z_re = ( tmp_re * tmp_re - tmp_im * tmp_im ) - const_re;
         z_im = ( 2 * abs(tmp_re * tmp_im) ) - const_im;
         cond = (abs(tmp_re) + abs(tmp_im)) * (abs(tmp_re) + abs(tmp_im));
+        ++cnt;
       } while (cnt < iterations && cond <= 4.0f);
-      output[x+y*width] = cnt;
+      output[x + (y - offset) * width] = cnt;
     }
   }
 )__";
 
 // TODO: fix types and arguments
 constexpr const char* julia_kernel = R"__(
-  __kernel void calculate_fractal(__global float* config,
-                                  __global int*   output) {
-    float offset = config[7];
-    float rows   = config[8];
+  __kernel void calculate_fractal(__global float*    config,
+                                  __global unsigned short* output) {
+    unsigned offset = (unsigned) config[7];
+    unsigned rows   = (unsigned) config[8];
     unsigned x = get_global_id(0);
     unsigned y = get_global_id(1);
     if (y >= offset && y < (offset + rows)) {
       unsigned iterations = config[0];
-      unsigned width = config[1];
+      unsigned width  = config[1];
       unsigned height = config[2];
       float min_re = config[3];
       float max_re = config[4];
       float min_im = config[5];
       float max_im = config[6];
-      float re_factor = (max_re-min_re)/(width-1);
-      float ifactor_ = (max_im-min_im)/(height-1);
+      float re_factor = (max_re - min_re) / (width  - 1);
+      float im_factor = (max_im - min_im) / (height - 1);
       float z_re = min_re + x*re_factor;
-      float z_im = max_im - y*ifactor_;
+      float z_im = max_im - y*im_factor;
       float const_re = z_re;
       float const_im = z_im;
       unsigned cnt = 0;
@@ -222,11 +223,12 @@ constexpr const char* julia_kernel = R"__(
       do {
         auto tmp_re = z_re;
         auto tmp_im = z_im;
-        z_re = ( tmp_re*tmp_re - tmp_im*tmp_im ) - const_re;
+        z_re = ( tmp_re * tmp_re - tmp_im*tmp_im ) - const_re;
         z_im = ( 2 * abs(tmp_re * tmp_im) ) - const_im;
         cond = (abs(tmp_re) + abs(tmp_im)) * (abs(tmp_re) + abs(tmp_im));
+        ++cnt;
       } while (cnt < iterations && cond <= 4.0f);
-      output[x+y*width] = cnt;
+      output[x + (y - offset) * width] = cnt;
     }
   }
 )__";
